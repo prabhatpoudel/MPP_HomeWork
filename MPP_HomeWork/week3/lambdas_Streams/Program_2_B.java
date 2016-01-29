2683a4df7bafbc8feb4793959f3014085e80daa5
Prabhat Poudel <prabhat.poudl@gmail.com>
2016-01-28 20:45:13 -0600

    Signed-off-by: Prabhat Poudel <prabhat.poudl@gmail.com>

--- MPP_HomeWork/week3/lambdas_Streams/Program_2_B.java
+++ MPP_HomeWork/week3/lambdas_Streams/Program_2_B.java
@@ -1,17 +1,27 @@
 package lambdas_Streams;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Program_2_B {
 	public static void main(String args[])
 	{
-		
+		ArrayList<String> list = new ArrayList<String>() ;
+		list.add("This");
+		list.add("Test");
+		list.add("List");
+		System.out.println(countWords(list,'s','i',4));
 	}
 	
-	public int countWords(List<String> words, char c, char d, int len)
+	public static int countWords(List<String> words, char c, char d, int len)
 	{
-		System.out.println( words.stream().filter(s-> s.contains(""+c)).filter(s-> !s.contains(""+d)).filter(s->s.length() == len));
-		return len;
+/*		words.stream()
+		.filter(s-> s.contains(""+c))
+		.filter(s-> !s.contains(""+d))
+		.filter(s->s.length() == len)
+		.forEach(System.out::println);*/
+		 int wordCount=(int) (words.stream().filter(s-> s.contains(""+c)).filter(s-> !s.contains(""+d)).filter(s->s.length() == len)).count();
+		return wordCount;
 		
 	}
 
